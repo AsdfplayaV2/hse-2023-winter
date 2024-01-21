81b623ecf65d13f96a21d4adfd801f6fa3785555
Willy Matthew Xamountry <willy.xamountry@googlemail.com>
2024-01-21 23:50:01 +0000

    frontend init

--- flaskbackend/app.py
+++ flaskbackend/app.py
@@ -5,15 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # SQLite configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasklist.db'
 db = SQLAlchemy(app)
 
 # Your routes and models go here
-@app.route('/')
-def hello():
-    return 'Hello, Flask in Docker!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
-
